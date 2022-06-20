22674a00e21492be7c04e083b19846fd69162b9d
unel <enee.prog@gmail.com>
2022-06-20 23:34:33 +0200

    add presentation of prcice

--- discordBot/currencyPresentation.js
+++ discordBot/currencyPresentation.js
@@ -3,7 +3,7 @@ const path = require('path');
 const draw = require('../tools/drawchart.js');
 async function currencyPresentation(channel, money, client) {
 	const img = draw(money.id, client);
-	let price = client.add(['priceEur', money.id]);
+	const info = await client.add(['info', money.id]);
 	const embed = new Discord.MessageEmbed();
 	embed.setTitle('information sur ' + money.name + ' à ' + new Date().getHours() + ':' + new Date().getMinutes());
 	embed.setFooter({ text: 'ces donnée peuvent être incorrecte' });
@@ -12,15 +12,9 @@ async function currencyPresentation(channel, money, client) {
 		{ name: 'symbole', value: money.symbol },
 		{ name: 'id', value: money.id }
 	);
-	if (money.market_cap_rank) {
-		embed.addFields(
-			{ name: 'marketcap', value: money.market_cap_rank.toString() }
-		);
-	}
-	price = await price;
-	embed.addField('prix', await price.toFixed(2));
 	embed.setThumbnail(money.large);
 	embed.setImage('attachment://image.png');
+	embed.addFields(constuctFields(info));
 	channel.send({
 		embeds: [embed],
 		files: [{
@@ -28,5 +22,37 @@ async function currencyPresentation(channel, money, client) {
 			name: 'image.png'
 		}]
 	});
+	client.add(['info', money.id]);
 }
+
+function constuctFields(info) {
+	const fields = [];
+	let sate;
+	let signe;
+	if (info.market_data.price_change_percentage_24h >= 20) {
+		signe = '+';
+		sate = ':arrow_double_up:';
+	} else if (info.market_data.price_change_percentage_24h > 0) {
+		signe = '+';
+		sate = ':arrow_up:';
+	} else if (info.market_data.price_change_percentage_24h < 20) {
+		signe = '-';
+		sate = ':arrow_double_down:';
+	} else if (info.market_data.price_change_percentage_24h < 0) {
+		signe = '-';
+		sate = ':arrow_down:';
+	} else {
+		signe = '';
+		sate = ':radio_button:';
+	}
+	if (info.market_data.current_price.eur) {
+		fields.push({ name: 'prix', value: info.market_data.current_price.eur + '€ (' + signe + info.market_data.price_change_percentage_24h.toFixed(2) + '%)' + sate });
+	}
+	if (info.links.homepage[0] != '') {
+		fields.push({ name: 'site web', value: info.links.homepage[0] });
+	}
+	return fields;
+}
+
+
 module.exports = currencyPresentation;

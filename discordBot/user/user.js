27ecd9b02c0c0d77b61ec2c2b8aba0d326603f12
unel <enee.prog@gmail.com>
2022-06-21 17:19:46 +0200

    function buy and sell / create history

--- discordBot/user/user.js
+++ discordBot/user/user.js
@@ -5,7 +5,10 @@ class user {
 		this.tag = tag;
 		this.cash = 1000;
 		this.walet = [['bitcoin', 1000]];
+		// this.history = [date, [name,quatity]]
+		this.history = [['bitcon', 1], new Date];
 	}
+
 	async toPresent(CoinGecko, channel) {
 		console.log(this.walet);
 		const embed = new MessageEmbed();
@@ -22,5 +25,35 @@ class user {
 		console.log(CoinGecko);
 		channel.send({ embeds: [embed] });
 	}
+
+	async buy(CoinGecko, channel, name, quantity) {
+		const price = await CoinGecko.add(['priceUsd', name]);
+		const total = price * quantity;
+		if (this.cash < total) {
+			channel.send('vous n\'avez pas ' + total + '$');
+			return (false);
+		} else {
+			this.cash -= total;
+			this.walet.push([name, quantity]);
+			this.history.push([new Date, this.walet]);
+			this.toPresent(CoinGecko, channel);
+		}
+	}
+
+	async sell(CoinGecko, channel, name, quantity) {
+		const price = await CoinGecko.add(['priceUsd', name]);
+		const total = price * quantity;
+		if (this.walet[name] < quantity) {
+			channel.send('vous n\'avez pas ' + quantity + ' ' + name);
+			return (false);
+		} else {
+			this.cash += total;
+			this.walet.splice(name, quantity);
+			this.history.push([new Date, this.walet]);
+			this.toPresent(CoinGecko, channel);
+
+		}
+	}
+
 }
 module.exports = user;

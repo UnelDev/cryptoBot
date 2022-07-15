4df6f33f21e53d422784cc3cb7fe8926f50ebb0a
unel <enee.prog@gmail.com>
2022-07-15 22:25:18 +0200

    add help in limit & stop sell

--- discordBot/help.js
+++ discordBot/help.js
@@ -1,7 +1,7 @@
 const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 
 function helpMenu(channel) {
-	const embed = new MessageEmbed;
+	const embed = new MessageEmbed();
 	embed.setTitle('aide')
 		.setDescription('PCT est un bot qui permet d\'investire de facon factice dans la crypto !\npour plus dinformation cliquer sur les bouton suivant');
 	const row = new MessageActionRow();
@@ -35,18 +35,63 @@ function helpMenu(channel) {
 			.setLabel('taxes')
 			.setStyle('PRIMARY')
 	);
+	const row2 = new MessageActionRow();
+	row2.addComponents(
+		new MessageButton()
+			.setCustomId('help_limitStopSell')
+			.setLabel('aide sur les limite et srop sell')
+			.setStyle('PRIMARY')
+	);
 	channel.send({
 		embeds: [embed],
-		components: [row]
+		components: [row, row2]
 	});
 }
 function helpMenuEdit(editable) {
-	const embed = new MessageEmbed;
+	const embed = new MessageEmbed();
 	embed.setTitle('aide')
 		.setDescription('PCT est un bot qui permet d\'investire de facon factice dans la crypto !\npour plus dinformation cliquer sur les bouton suivant');
+	const row = new MessageActionRow();
+	row.addComponents(
+		new MessageButton()
+			.setCustomId('help_legal')
+			.setLabel('legal')
+			.setStyle('PRIMARY')
+	);
+	row.addComponents(
+		new MessageButton()
+			.setCustomId('help_global')
+			.setLabel('vos debut')
+			.setStyle('PRIMARY')
+	);
+	row.addComponents(
+		new MessageButton()
+			.setCustomId('help_cmd')
+			.setLabel('commande')
+			.setStyle('PRIMARY')
+	);
+	row.addComponents(
+		new MessageButton()
+			.setCustomId('help_tips')
+			.setLabel('tips')
+			.setStyle('PRIMARY')
+	);
+	row.addComponents(
+		new MessageButton()
+			.setCustomId('help_taxes')
+			.setLabel('taxes')
+			.setStyle('PRIMARY')
+	);
+	const row2 = new MessageActionRow();
+	row2.addComponents(
+		new MessageButton()
+			.setCustomId('help_limitStopSell')
+			.setLabel('aide sur les limite et srop sell')
+			.setStyle('PRIMARY')
+	);
 	editable.edit({
 		embeds: [embed],
-		components: createRow()
+		components: [row, row2]
 	});
 }
 
@@ -63,12 +108,14 @@ function helpInteractionRepleay(message, editable) {
 		taxes(editable);
 	} else if (message == 'global') {
 		global(editable);
+	} else if (message == 'limitStopSell') {
+		limitStopSell(editable);
 	}
 
 }
 
 function legal(editable) {
-	const embed = new MessageEmbed;
+	const embed = new MessageEmbed();
 	embed.setTitle('information legal')
 		.setDescription('PCT est un bot qui permet d\'investire de facon factice dans la crypto !\n')
 		.setTimestamp()
@@ -81,7 +128,7 @@ function legal(editable) {
 }
 
 function command(editable) {
-	const embed = new MessageEmbed;
+	const embed = new MessageEmbed();
 	embed.setTitle('aide sur les commande')
 		.setDescription('voici une desccription des diferante commande toute les commande doive etre precédée du prefix')
 		.addField('info {paramètre (id)}', 'permet d\'optenire des information sur le parametre lest information peuve etre : le nom, une courbe de son evolution sur 24h, son id (utiliser dans les command), son symbole (eur ou BTC) par exemple, et ses diverante evolution de prix a plusieur echelle de temps. apres ca il vous sera proposée dans acheter ou d\'en revendre')
@@ -90,34 +137,22 @@ function command(editable) {
 		.addField('presentation ou p', 'presente l\'etat du marchée avec plusieur crypto tel que: bitcoin, ethereum, binancecoin, iShares MSCI World ETF Tokenized Stock Defichain')
 		.addField('walet', 'afice les information sur votre walet, peut aussi si cella est pertinant tracée une courbe')
 		.addField('help', 'afiche cette aide');
-	const row = new MessageActionRow();
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_')
-			.setLabel('menu d\'aide')
-			.setStyle('PRIMARY')
-	);
+
 	editable.edit({
 		embeds: [embed],
-		components: [row]
+		components: createRow()
 	});
 }
 
 function tips(editable) {
-	const embed = new MessageEmbed;
+	const embed = new MessageEmbed();
 	embed.setTitle('quelque tips !')
 		.setDescription('je vais vous aprendre ici quelque tips sur les crypto est sur moi PCT')
 		.addField('courbe', 'lors ce que vous faite la commande p ou prensentation une courbe de la monnaie s\'afiche, elle est calculer sur 24h. si vous voulez en voir plus vous pouvez cliquer sur graphique autre periode');
-	const row = new MessageActionRow();
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_')
-			.setLabel('menu d\'aide')
-			.setStyle('PRIMARY')
-	);
+
 	editable.edit({
 		embeds: [embed],
-		components: [row]
+		components: createRow()
 	});
 }
 
@@ -128,75 +163,47 @@ function taxes(editable) {
 		.addField('Taxe d\'achat ', '2% elle s\'applique lors ce que vous cliquer sur un bouton acheter.')
 		.addField('Taxe d\'échange ou spread', 'n\'est jamais dévoilé dans la vrais vie. C\'est le fait de gonfler artificiellement le prix d\'une monnaie. Dans PCT il est affiché et il s\'applique que lors des échanges')
 		.addField('utilitée', 'on pourais ce demandée pourquoi des taxes doive etre aplquée ! les taxes dan la vrais vie serve a payer les platforme ! PCT se veut le plus realiste posible c\'est pourquoi elle existe ici aussi, elle serve aussi a finacée les efet levier mais chut ca arive bientôt');
-	const row = new MessageActionRow();
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_')
-			.setLabel('menu d\'aide')
-			.setStyle('PRIMARY')
-	);
+
 	editable.edit({
 		embeds: [embed],
-		components: [row]
+		components: createRow()
 	});
 }
 
 function global(editable) {
-	const embed = new MessageEmbed;
+	const embed = new MessageEmbed();
 	embed.setTitle('Aide global')
 		.setDescription('ici je vais vous espliquée comment je fonctionne !')
 		.addField('vos debut', 'au debut PCT vous donne 1000$ (oui c\'est becoups) apres vous pouvez faire ce que vous voulée avec ! le but de ce je d\'avoir la plus haute capitalisation !')
 		.addField('la suite', 'bon c\'est bien beaux de dire qu\'il faut gagner de l\'argent mais comment ?')
 		.addField('comment gagner de l\'argent ?', 'pour gagner de l\'argent il faut trouver une crypto qui va beaucoups monter pour cella les courbe vont etre utile. Vous investiser donc une partie de votre argent dedans.Les taxes vont etre prise (voir aide sur es taxes) il faut donc que la crypto monte assez pour pouvoir remboursée et votre investisemment et les taxes !');
+	editable.edit({
+		embeds: [embed],
+		components: createRow()
+	});
+}
+
+function limitStopSell(editable) {
+	const embed = new MessageEmbed();
+	embed.setTitle('aide sur les limit sell et les stop sell')
+		.setFooter({ text: 'ces action demmande beaucoup de resource a etre calculée, n\'en abusée pas ! ' })
+		.setField('utilité', 'les limite est les stop sell sont utile pour des personne un peut experimentée. c\'est un moyen de vendre automatiquemment vos crypto lors ce qu\'elle arrive a un certain prix')
+		.addField('sell stop', 'permet de configurer une limite a laquelle sera vendus vos crypto si leur prix est **inferieur** a cette limite')
+		.addField('limit sell ', 'permet de configurer une limite a laquelle sera vendus vos crypto si leur prix est **superieur** a cette limite')
+		.addField('exemple limit sell', 'j\'achete 1 bitcoin a 20 000$ je configure un limit sell a 19 000$ lors ce que le prix du bitcoin passe a 18 999$ il sera vendu en totalitée automatiquement par le bot')
+		.addField('exemple stop sell', 'j\'achete 1 bitcoin a 20 000$ je configure un stop sell a 21 000$ lors ce que le prix du bitcoin passe a 21 000,00...1$ il sera vendu en totalitée automatiquement par le bot')
+		.addField('limitation', 'bon pour ne pas se mentir les stop et limit sell sont tres gourment en requete, je suis limitée a 50 requete minite et chaque limit et stop sell sont verifier toute les deux minute il demande donc une requete pour chaque limite ou stop sell. \n donc svp ne configurer pas 200 limite/stop sell sinon je devrais les limitée !');
+	editable.edit({
+		embeds: [embed],
+		components: createRow()
+	});
+}
+function createRow() {
 	const row = new MessageActionRow();
 	row.addComponents(
 		new MessageButton()
 			.setCustomId('help_')
 			.setLabel('menu d\'aide')
-			.setStyle('PRIMARY')
-	);
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_taxes')
-			.setLabel('taxes')
-			.setStyle('PRIMARY')
-	);
-	editable.edit({
-		embeds: [embed],
-		components: [row]
-	});
-}
-
-function createRow() {
-	const row = new MessageActionRow();
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_legal')
-			.setLabel('legal')
-			.setStyle('PRIMARY')
-	);
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_global')
-			.setLabel('vos debut')
-			.setStyle('PRIMARY')
-	);
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_cmd')
-			.setLabel('commande')
-			.setStyle('PRIMARY')
-	);
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_tips')
-			.setLabel('tips')
-			.setStyle('PRIMARY')
-	);
-	row.addComponents(
-		new MessageButton()
-			.setCustomId('help_taxes')
-			.setLabel('taxes')
 			.setStyle('PRIMARY')
 	);
 	return [row];

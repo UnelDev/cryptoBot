1e69738e04e46e6378f56b1d4c53de9c1a4a2a7f
unel <enee.prog@gmail.com>
2022-07-19 12:13:12 +0200

    fix bug in gestion limit sell

--- discordBot/user/gestion/gestionLimitSell.js
+++ discordBot/user/gestion/gestionLimitSell.js
@@ -7,37 +7,37 @@ async function gestionLimitSell(user, channel, dateStart) {
 	let limitSell = '';
 	let sleepLimit = [Promise.resolve];
 	let sleepStop = [Promise.resolve];
-	if (typeof user.StopSell != 'undefined' && user.StopSell != []) {
-		sleepStop = user.StopSell.map(element => {
-			StopSell += ', ' + element[0];
-			creatorStop.push(['stopSellStop_' + element[0], 'aréter le stopSell de ' + element[0]]);
+	if ((typeof user.limitSell != 'undefined') && (user.limitSell != []) && (user.limitSell.length != 0)) {
+		sleepStop = user.limitSell.map(element => {
+			StopSell += ', ' + element[0].toString();
+			creatorStop.push(['stopLimitSell_' + element[0].toString(), 'aréter le limit sell de ' + element[0].toString()]);
 		});
 		await Promise.all(sleepStop);
 		// send message for limit sell
 		const embed = new MessageEmbed();
-		embed.setTitle('voici vos stop sell')
-			.addField('limit sell', limitSell)
+		embed.setTitle('voici vos limit sell')
+			.addField('limit sell', StopSell.replace(', ', '').toString())
 			.setFooter({ text: (new Date() - dateStart).toString() + 'ms' });
 		channel.send({
 			embeds: [embed]
 		});
 		createButton(creatorStop, channel);
 	}
-	if (typeof user.StopSell != 'undefined' && user.StopSell != []) {
-		sleepLimit = user.limitSell.map(element => {
-			limitSell += ', ' + element[0];
-			creatorLimit.push(['stopSellStop_' + element[0], 'aréter le stopSell de ' + element[0]]);
+	if ((typeof user.sellStop != 'undefined') && (user.sellStop != []) && (user.sellStop.length != 0)) {
+		sleepLimit = user.sellStop.map(element => {
+			limitSell += ', ' + element[0].toString();
+			creatorLimit.push(['stopSellStop_' + element[0].toString(), 'aréter le stop sell de ' + element[0].toString()]);
 		});
 		await Promise.all(sleepLimit);
-		// send message for stop sell
+		// send message for limit sell
 		const embed = new MessageEmbed();
 		embed.setTitle('voici vos stop sell')
-			.addField('limit sell', StopSell)
+			.addField('stop sell', limitSell.replace(', ', '').toString())
 			.setFooter({ text: (new Date() - dateStart).toString() + 'ms' });
 		channel.send({
 			embeds: [embed]
 		});
-		creatorLimit(creatorStop, channel);
+		createButton(creatorLimit, channel);
 	}
 	if (StopSell == '' && limitSell == '') {
 		const row = new MessageActionRow();
@@ -66,13 +66,13 @@ function createButton(creator, channel) {
 		if (numberInWhile == 0) {
 			buttonArray.push(new MessageActionRow());
 			buttonArray[buttonArray.length - 1] = buttonArray[buttonArray.length - 1].addComponents(new MessageButton()
-				.setCustomId(creator[parseButon][0])
-				.setLabel(creator[parseButon][1])
+				.setCustomId(creator[parseButon][0].toString())
+				.setLabel(creator[parseButon][1].toString())
 				.setStyle('PRIMARY'));
 		} else {
 			buttonArray[buttonArray.length - 1] = buttonArray[buttonArray.length - 1].addComponents(new MessageButton()
-				.setCustomId(creator[parseButon][0])
-				.setLabel(creator[parseButon][1])
+				.setCustomId(creator[parseButon][0].toString())
+				.setLabel(creator[parseButon][1].toString())
 				.setStyle('PRIMARY'));
 		}
 		if (numberInWhile == 4) {
